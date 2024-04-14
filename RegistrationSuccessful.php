7b08ea32f1e143d70d7988e1f54e1f15dd6ab721
percymiltier <percymiltier@gmail.com>
2024-04-14 14:58:05 -0400

    Finished HTML for login page Made a redirect for after registering an account

---
+++ RegistrationSuccessful.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1000px, initial-scale=1.0">
+    <title>Registration Successful!</title>
+    <link href="/stylesheet.css" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+    <!-- main -->
+    <div class="main">
+        <p>
+            Registration successful!<br><br>
+            <button class="yesbutton" ahref="/login.php" style="width: 50%;">Login</button>
+        </p>
+    </div>
+</body>
--- login.php
+++ login.php
@@ -6,20 +6,43 @@
 </head>
 
 <body>
-    <div style="display:flex; flex-flow:column;">
-        <!-- button to use as a guest -->
-        <button class="yesbutton" ahref="/guest.html">View as Guest</button>
+    <!-- main -->
+    <div class="main">
+        <div style="display:flex; flex-flow:column;">
+            <!-- button to use as a guest -->
+            <button class="yesbutton" ahref="/guest.html" style="width:50%;">View as Guest</button>
 
-        <div style="display:flex; flex-flow:row;">
-            <div style="display:flex; flex-flow:column;">
-                <!-- User login -->
-                <p>
-                    Username: 
-                </p>
-            </div>
-            <div style="height:100%; border:#000 2px solid;"></div>
-            <div style="display:flex; flex-flow:column;">
-                <!-- Register a new account -->
+            <h1 style="color:#3E51A5; text-align:center;">OR</h1>
+    
+            <div style="display:flex; flex-flow:row;">
+                <div style="display:flex; flex-flow:column; justify-content: center;">
+                    <!-- User login -->
+                    <p>Login:</p>
+                    <p >
+                        Username: 
+                        <input type="text" id="username">
+                    </p>
+                    <p>
+                        Password:
+                        <input type="password" id="password">
+                    </p>
+                    <button class="yesbutton" ahref="" style="width: 40%;">Login</button>
+                </div>
+                <div style="height: 200px; border:1px solid #3E51A5; margin:15px"></div>
+                <div style="display:flex; flex-flow:column; justify-content: center;">
+                    <!-- Register -->
+                    <p>Register:</p>
+                    <p>
+                        Username: 
+                        <input type="text" id="username">
+                    </p>
+                    <p>
+                        Password:
+                        <input type="password" id="password">
+                    </p>
+                    <button class="yesbutton" ahref="" style="width: 40%;">Register</button>
+                    
+                </div>
             </div>
         </div>
     </div>
