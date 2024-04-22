7680df986ec95e95b24d90a1a984ebdc08b5dc5f
Jonathan Windsor <89407955+jonwindsor@users.noreply.github.com>
2024-04-22 11:51:34 -0400

    Created AddSuccessful.php

---
+++ AddSuccessful.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1000px, initial-scale=1.0">
+    <title>Pokemon added Successfully!</title>
+    <link href="/Pokedex/stylesheet.css" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+    <!-- main -->
+    <div class="main">
+        <p>
+            Pokemon added successfully!<br><br>
+            <a href="/Pokedex/databaseviewerU.php">View database</a>
+        </p>
+    </div>
+</body>
