68c64f619e8cac8105fa8e0bdda51a389fdd94f9
Jonathan Windsor <89407955+jonwindsor@users.noreply.github.com>
2024-04-22 11:47:59 -0400

    Dropdown Updates

--- addnew.php
+++ addnew.php
@@ -11,17 +11,54 @@
         <h1>Add a new record:</h1>
         <p style="text-align: right;">
             Pokemon: <input type="text" id="pokemonName"><br>
-            Type (type NULL if no secondary type): <input type="text" id="type1"> <input type="text" id="type2"><br>
-            Status (Normal/Legendary/Mythical):  <label for="status">Choose a status:</label>
-
-            <select name="status" id="status">
+            Type 1: <select name="type1" id="type1">
+              <option value="grass">Grass</option>
+              <option value="fire">Fire</option>
+              <option value="water">Water</option>
+              <option value="electric">Electric</option>
+              <option value="ice">Ice</option>
+              <option value="posion">Posion</option>
+              <option value="ground">Ground</option>
+              <option value="flying">Flying</option>
+              <option value="psychic">Psychic</option>
+              <option value="bug">Bug</option>
+              <option value="rock">Rock</option>
+              <option value="ghost">Ghost</option>
+              <option value="dragon">Dragon</option>
+              <option value="dark">Dark</option>
+              <option value="steel">Steel</option>
+              <option value="fairy">Fairy</option>
+                
+            </select> <br>
+            Type 2: <select name="type2" id="type2">
+              <option value="notype2">NULL</option>
+              <option value="grass">Grass</option>
+              <option value="fire">Fire</option>
+              <option value="water">Water</option>
+              <option value="electric">Electric</option>
+              <option value="ice">Ice</option>
+              <option value="posion">Posion</option>
+              <option value="ground">Ground</option>
+              <option value="flying">Flying</option>
+              <option value="psychic">Psychic</option>
+              <option value="bug">Bug</option>
+              <option value="rock">Rock</option>
+              <option value="ghost">Ghost</option>
+              <option value="dragon">Dragon</option>
+              <option value="dark">Dark</option>
+              <option value="steel">Steel</option>
+              <option value="fairy">Fairy</option>
+            </select> <br>
+            
+            Status: <select name="status" id="status">
               <option value="normal">Normal</option>
               <option value="legendary">Legendary</option>
               <option value="mythical">Mythical</option>
             </select> <br>
-            Trainer Name: <input type="text" id="pokemonTrainer"><br>
-            Region: <label for="region">Choose a region:</label>
-            <select name="region" id="region">
+            
+            Trainer Name: <input type="text" id="trainer_name"><br>
+            
+            Region: <select name="region" id="region">
               <option value="kanto">Kanto</option>
               <option value="johto">Johto</option>
               <option value="hoenn">Hoenn</option>
@@ -32,6 +69,7 @@
               <option value="galar">Galar</option>
               <option value="paldea">Paldea</option>
             </select><br>
+            
             Held Item (type NULL if there is none): <input type="text" id="heldItem"><br>
         </p>
         <p>
